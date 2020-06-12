ff0c97a74a6b8fa3761e2c64b1e3e0b390beb4d3
alexviu <viu.alex771@gmail.com>
2020-06-12 14:13:09 +0200

    languages-front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LanguagesComponent } from './languages/languages.component';
+import { ContainerComponent } from './container/container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'languages', component: LanguagesComponent },
+  { path: 'containers', component: ContainerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
